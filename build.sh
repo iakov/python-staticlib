d928dea1b3edf0e9d831245b0a49ae8ee733a42b
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-01-18 01:06:18 +0300

    Initial test

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -xuevo pipefail
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-pushd $SCRIPT_DIR
-make -f ../Makefile.static -C cpython.git/ -j 4 $*
+SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+INSTALL_DIR="${INSTALL_DIR:-${SCRIPT_DIR}/install}"
+pushd "$SCRIPT_DIR"
+make -f ../Makefile.static -C cpython.git/ -j 4 PREFIX=${INSTALL_DIR} $*
+export PKG_CONFIG_PATH="${INSTALL_DIR}/lib/pkgconfig"
+gcc test.c -o test.exe $(pkg-config --cflags python-3.8) $(pkg-config --libs --static python-3.8)
+./test.exe
 popd
--- test.c
+++ test.c
@@ -1,4 +1,13 @@
 #include "Python.h"
+const char *code[] = {
+	"print('Hello world')"
+	, NULL
+};
+
 int main() {
+	Py_Initialize();
+	for (const char **p = code; *p; ++p) {
+		PyRun_SimpleString(*p);
+	}
 	return 0;
 }

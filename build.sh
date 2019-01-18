6c07817c634d5b816b483e37ef9224a2152b951e
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-01-18 10:23:54 +0300

    Build test with proper workaround trick for pkg-config

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ INSTALL_DIR="${INSTALL_DIR:-${SCRIPT_DIR}/install}"
 pushd "$SCRIPT_DIR"
 make -f ../Makefile.static -C cpython.git/ -j 4 PREFIX=${INSTALL_DIR} $*
 export PKG_CONFIG_PATH="${INSTALL_DIR}/lib/pkgconfig"
-gcc test.c -o test.exe $(pkg-config --cflags python-3.8) $(pkg-config --libs --static python-3.8)
+gcc test.c -o test.exe $(pkg-config --cflags python-3.8) $(pkg-config --libs python-3.8) $(pkg-config --libs-only-l --static python-3.8)
 ./test.exe
 popd

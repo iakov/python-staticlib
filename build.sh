1201c956470033d577b060dce16f7a18e41b3e42
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-01-18 16:13:07 +0300

    Draft!!!

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -xuevo pipefail
-SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+SCRIPT_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 INSTALL_DIR="${INSTALL_DIR:-${SCRIPT_DIR}/install}"
 pushd "$SCRIPT_DIR"
 make -f ../Makefile.static -C cpython.git/ -j 4 PREFIX=${INSTALL_DIR} $*
-export PKG_CONFIG_PATH="${INSTALL_DIR}/lib/pkgconfig"
-gcc test.c -o test.exe $(pkg-config --cflags python-3.8) $(pkg-config --libs python-3.8) $(pkg-config --libs-only-l --static python-3.8)
 ./test.exe
 popd

206d778382fc7882dca10e84472685dd98e5d718
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-01-18 16:13:07 +0300

    Draft!!!

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xuevo pipefail
-SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+SCRIPT_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 INSTALL_DIR="${INSTALL_DIR:-${SCRIPT_DIR}/install}"
 pushd "$SCRIPT_DIR"
 make -f ../Makefile.static -C cpython.git/ -j 4 PREFIX=${INSTALL_DIR} $*
